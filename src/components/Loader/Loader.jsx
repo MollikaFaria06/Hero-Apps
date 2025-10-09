f6978ed2940dd8d61cfc4eeb4fe9734cb1a108d8
Faria Mollika <149615629+MollikaFaria06@users.noreply.github.com>
2025-10-09 13:11:54 +0600

    added loader color to purple

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Loader = () => (
-  <div className="flex justify-center items-center min-h-screen bg-amber-50">
+  <div className="flex justify-center items-center min-h-screen bg-purple-200">
     <div className="w-12 h-12 border-4 border-purple-600 border-t-transparent rounded-full animate-spin"></div>
   </div>
 );
